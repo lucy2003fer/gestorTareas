f04fe38b5ae65cef0df027320873c7d361a81d7f
lucy2003fer <lucy48fernada@gmail.com>
2024-08-23 09:58:44 -0500

    inicializando proyecto de gestor de tareas

--- gestor/views.py
+++ gestor/views.py
@@ -3,3 +3,10 @@ from .models import Tarea
 from .form import TareaForm
 
 # Create your views here.
+
+def crear(request):
+    if request.method == 'POST':
+        form = TareaForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('listar')
